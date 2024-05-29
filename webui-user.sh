fbf1fc37427bf13634a42f56c4397e70de85d46e
Steve Wang <steve@haldi.com>
2024-05-29 23:42:30 +0800

    settings

--- webui-user.sh
+++ webui-user.sh
@@ -10,16 +10,16 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate --api"
 
 # python3 executable
-#python_cmd="python3"
+python_cmd="python3.10"
 
 # git executable
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir=".venv"
 
 # script to launch to start the app
 #export LAUNCH_SCRIPT="launch.py"
